f91dda372d30c9c504ec1142d9a61f4beff24d48
Steffen <sir.butcher93@gmail.com>
2021-04-17 12:53:46 +0200

    init

---
+++ app/src/main/java/de/smetzger/nfcalarmclock/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.smetzger.nfcalarmclock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
